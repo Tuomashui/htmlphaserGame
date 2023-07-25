4acb1e60d59c4d5c241fd3d509a42ee72b597471
Tuomashui <122463016+Tuomashui@users.noreply.github.com>
2023-07-25 12:49:09 +0300

    letsgoo

---
+++ scenes/BootGameScene.js
@@ -0,0 +1,18 @@
+export class BootGameScene extends Phaser.Scene {
+    constructor(){
+        super("BootGameScene");
+    }
+    create(){
+        this.cameras.main.setBackgroundColor('#000000');
+        this.add.text(50,50, "Loading game...");
+        let text = this.add.text(500, 300, 'REACH 7 POINTS!!', {
+            fontFamily: 'Arial',
+            fontSize: '64px',
+            color: `rgb(${0}, ${0}, ${200})`
+        }).setOrigin(0.5);
+
+        this.time.delayedCall(3000, () => {
+            this.scene.start("PlayGameScene");
+    });
+}
+}
